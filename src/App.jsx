2e5b88408765ecb0b351820194b570e01f11134c
TahaImran <tahaimran.24@gmail.com>
2022-01-20 12:15:34 +0500

    styling updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,28 @@ function App() {
 
   return (
     <>
-      <Grid sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
+      <Grid sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
 
         <img src={Logo} width={70} alt="" />
-        <Typography variant="h2" sx={{ ml: 3, color: "#F58220" }}>Halaqahs</Typography>
+        <Typography variant="h3" sx={{ ml: 3, color: "#fff" }}>Youth Club Weekly Classes</Typography>
       </Grid>
 
 
       <Grid container sx={{ justifyContent: 'space-evenly', mt:2,mb:5 }} >
         <Single src='https://youthclub.pk/wp-content/uploads/2022/01/the-key.jpg'
-          title="The Key Halaqah"
+          title="The Key"
           speaker="Speaker: Khurram Alvi"
+          address='House # 93-B, St. 66, Sector F-11/4, Islamabad'
+          timings='Every Tuesday 8:00-9:00 PM'
           audience='Audience: Males only'
           href='https://goo.gl/maps/SHjSyEh9wnDRHU1Q8'
         />
 
         <Single src='https://youthclub.pk/wp-content/uploads/2022/01/the-light.jpg'
-          title="The Light Halaqah"
+          title="The Light"
           speaker="Speaker: Abu Saad"
+          address='House 279, street 100, I-8/4, Islamabad'
+          timings='Every Friday 8:00 PM'
           audience='Audience: Males only'
           href='https://maps.app.goo.gl/kQiNuUi7hN3EPq1u9'
         />
@@ -32,6 +36,8 @@ function App() {
         <Single src={Greatest}
           title="The Greatest Men"
           speaker="Speaker: Abu Saad"
+          address='House# 15, Street# 56, Sector F-6/4, Islamabad'
+          timings='Every Saturday 5:45 PM'
           audience='Audience: Males only'
           href='https://maps.app.goo.gl/vGyV1ck7jtuLMiXj7'
         />
--- src/Single.jsx
+++ src/Single.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Grid, Typography } from '@mui/material'
 import Button from '@mui/material/Button';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import {LocationOn,AccessTime,HomeOutlined} from '@mui/icons-material';
 import Link from '@mui/material/Link';
-
+import './App.css'
 function Single(props) {
-    return <Grid item sx={{mt:5}} >
-        <Typography sx={{ fontWeight: 'bold' }} variant="h4">
-            {props.title}
-        </Typography>
-        <img src={props.src} width={350} alt="" />
-        <Typography sx={{mt:1}}  variant="h6">{props.speaker}</Typography>
-        <Typography sx={{mt:1}}  variant="h6">{props.audience}</Typography>
-        <Link  sx={{ textDecoration: 'none' }} target='_blank' href={props.href}> <Button sx={{mt:1}} variant="contained" color="success" size='large' startIcon={<LocationOnIcon />}  > Find Us </Button>
-        </Link>
-    </Grid>
+    return (
+        <Grid item sx={{ mt: 5 }} >
+            <Typography sx={{ fontWeight: 'semiBold' }} variant="h4">
+                {props.title}
+            </Typography>
+            <img width={320}  src={props.src} alt="" />
+            <Grid item  sx={{width :' 80%'}}>
+                <Typography sx={{ mt: 1 }} variant="h6">{props.speaker}</Typography>
+
+                 <Typography sx={{ mt: 1,display: 'block' }} variant="p" >{props.address}</Typography>
+                <Typography sx={{ mt: 1, display: 'block' }} variant="p">{props.timings}</Typography> 
+
+
+                <Typography sx={{ mt: 1 }} variant="h6">{props.audience}</Typography>
+                <Link sx={{ textDecoration: 'none' }} target='_blank' href={props.href}> <Button sx={{ mt: 1, width: 200 }} variant="contained" color="success" size='large' startIcon={<LocationOn />}>Pin</Button>
+                </Link>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default Single;
